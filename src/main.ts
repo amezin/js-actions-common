d4386f32ad0f4a56f3f904e6357b2fa3172fdc40
Aleksandr Mezin <mezin.alexander@gmail.com>
2025-06-15 16:08:28 +0300

    Report errors using `@actions/core`

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
+import { inspect } from 'node:util';
+import * as core from '@actions/core';
+
 async function main() {}
 
-main().catch(error => console.error(error));
+main().catch((error: unknown) => {
+    core.setFailed(String(error));
+    core.debug(inspect(error));
+});
